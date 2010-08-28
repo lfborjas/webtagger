9f5b97cde2c1a7b8bdfd34679601114f026daebb
lfborjas <luis.borjas@escolarea.com>
2010-08-27 21:29:12 -0600

    first draft working with tagthe

--- lib/webtagger.rb
+++ lib/webtagger.rb
@@ -3,9 +3,10 @@ require 'httparty_icebox'
 
 class Yahoo
     include HTTParty
+    include HTTParty::Icebox
     format :json
     base_uri "http://search.yahooapis.com/ContentAnalysisService/V1"
-    cache :store => 'memory', timeout => 1
+    cache :store => 'memory', :timeout => 1
     
     def self.tag(text, token)
         resp = post("/termExtraction", :query => {:appid => token, :context => text, :output=>'json'} )
@@ -18,12 +19,13 @@ end
 
 class Alchemy
     include HTTParty
+    include HTTParty::Icebox
     format :json
     base_uri "http://access.alchemyapi.com/calls/text"
-    cache :store => 'memory', timeout => 1
+    cache :store => 'memory', :timeout => 1
     
-    def self.tag(text)
-        resp = post("/", :query => {:text => text, :view=>'json'} )
+    def self.tag(text, token)
+        resp = post("/", :query => {:apikey => token, :text => text, :outputMode=>'json'} )
         return [] if resp['status'] == 'ERROR'
         return resp['keywords']
     end
@@ -31,16 +33,17 @@ end
 
 class Tagthe 
     include HTTParty
+    include HTTParty::Icebox
     format :json
     base_uri "http://tagthe.net/api"
-    cache :store => 'memory', timeout => 1
+    cache :store => 'memory', :timeout => 1
     
-    def self.tag(text, token)
-        resp = post("/", :query => {:apikey => token, :text => text, :outputMode=>'json'} )
-        if resp.has_key?('memes') and resp['memes'][0].has_key?('dimensions')\
-            and data['memes'][0]['dimensions'].has_key?('topic')
+    def self.tag(text)
+        resp = post("/", :query => {:text => text, :view=>'json'} )
+        if resp.has_key?('memes') and resp['memes'][0].has_key?('dimensions') \
+            and resp['memes'][0]['dimensions'].has_key?('topic')
             
-            return = data['memes'][0]['dimensions']['topic']
+            return resp['memes'][0]['dimensions']['topic']
         else
             return []
         end
@@ -48,16 +51,13 @@ class Tagthe
 end
 
 def tag(text, token="", service="tagthe")
-    #return [] unless ['tagthe', 'yahoo', 'alchemy', 'tagthe'].include?(service.strip.downcase)
     service = service.strip.downcase
     case
         when service == "yahoo":
             return Yahoo.tag(text, token)
-        when service == "tagthe":
-            return Tagthe.tag(text)
         when service == "alchemy":
             return Alchemy.tag(text, token)
         else
-            return []
+            return Tagthe.tag(text)
     end
 end
