a89d7de03dd45e8ba5e2599759f47e21ca88187f
lfborjas <luis.borjas@escolarea.com>
2010-08-28 20:33:56 -0600

    using a module now, created the get_token method to read tokens from ~/.webtagger and tested basic use cases, raising exceptions if the api calls go wrong

--- lib/webtagger.rb
+++ lib/webtagger.rb
@@ -1,63 +1,111 @@
+require 'fileutils'
 require 'httparty'
 require 'httparty_icebox'
 
-class Yahoo
-    include HTTParty
-    include HTTParty::Icebox
-    format :json
-    base_uri "http://search.yahooapis.com/ContentAnalysisService/V1"
-    cache :store => 'memory', :timeout => 1
-    
-    def self.tag(text, token)
-        resp = post("/termExtraction", :query => {:appid => token, :context => text, :output=>'json'} )
-        if resp.has_key?('ResultSet')
-            return resp['ResultSet']['Result'] || []
-        end
-        return []
-    end
-end
+module WebTagger
 
-class Alchemy
-    include HTTParty
-    include HTTParty::Icebox
-    format :json
-    base_uri "http://access.alchemyapi.com/calls/text"
-    cache :store => 'memory', :timeout => 1
+    SERVICES = ['yahoo', 'alchemy', 'tagthe']
     
-    def self.tag(text, token)
-        resp = post("/", :query => {:apikey => token, :text => text, :outputMode=>'json'} )
-        return [] if resp['status'] == 'ERROR'
-        return resp['keywords']
-    end
-end
-
-class Tagthe 
-    include HTTParty
-    include HTTParty::Icebox
-    format :json
-    base_uri "http://tagthe.net/api"
-    cache :store => 'memory', :timeout => 1
-    
-    def self.tag(text)
-        resp = post("/", :query => {:text => text, :view=>'json'} )
-        if resp.has_key?('memes') and resp['memes'][0].has_key?('dimensions') \
-            and resp['memes'][0]['dimensions'].has_key?('topic')
-            
-            return resp['memes'][0]['dimensions']['topic']
-        else
-            return []
+    class WebTaggerError < RuntimeError
+        attr :response
+        def initialize(resp)
+            @response = resp
         end
     end
-end
+    
+    def get_token(service="")
+        service = service.strip.downcase
+        conf = File.join(ENV['HOME'], '.webtagger')
+        return nil unless File.exist? conf
+        srvcs = {}
+        File.open(conf).each do |service_conf|
+            s, t = service_conf.split(/\s*=\s*/) rescue next
+            srvcs[s.strip.downcase] = t.strip
+        end
 
-def tag(text, token="", service="tagthe")
-    service = service.strip.downcase
-    case
-        when service == "yahoo":
-            return Yahoo.tag(text, token)
-        when service == "alchemy":
-            return Alchemy.tag(text, token)
+        return case 
+        when service == "all"
+            srvcs
+        when (SERVICES.include?(service) and srvcs[service])
+            srvcs[service]
         else
-            return Tagthe.tag(text)
+            nil
+        end
     end
-end
+
+    class Yahoo
+        include HTTParty
+        include HTTParty::Icebox
+        format :json
+        base_uri "http://search.yahooapis.com/ContentAnalysisService/V1"
+        cache :store => 'memory', :timeout => 1
+        
+        def self.tag(text, token)
+            raise "Token missing!" unless token
+            resp = post("/termExtraction", :query => {:appid => token, :context => text, :output=>'json'} )
+            if resp.has_key?('ResultSet')
+                return resp['ResultSet']['Result'] || []
+            else
+                raise WebTaggerError.new(resp), "Error in API call"
+            end
+        end
+    end
+
+    class Alchemy
+        include HTTParty
+        include HTTParty::Icebox
+        format :json
+        base_uri "http://access.alchemyapi.com/calls/text"
+        cache :store => 'memory', :timeout => 1
+        
+        def self.tag(text, token)
+            raise "Token missing!" unless token
+            resp = post("/TextGetRankedKeywords", :query => {:apikey => token, :text => text, :outputMode=>'json'} )
+            if resp['status'] != 'ERROR'
+                #it's a hash array of [{:text=>"", :relevance=>""}]
+                kws = []
+                resp['keywords'].each do |m|
+                    kws.push m["text"]
+                end
+                return kws
+            else
+                raise WebTaggerError.new(resp), "Error in API call"
+            end          
+        end
+    end
+
+    class Tagthe 
+        include HTTParty
+        include HTTParty::Icebox
+        format :json
+        base_uri "http://tagthe.net/api"
+        cache :store => 'memory', :timeout => 1
+        
+        def self.tag(text)
+            resp = post("/", :query => {:text => text, :view=>'json'} )
+            if resp.has_key?('memes') and resp['memes'][0].has_key?('dimensions') \
+                and resp['memes'][0]['dimensions'].has_key?('topic')
+                
+                return resp['memes'][0]['dimensions']['topic']
+            else
+                return []
+            end
+        end
+    end
+
+    def tag(text,service="tagthe",token=nil)
+        service = service.strip.downcase
+        token = get_token(service) unless token
+        return case
+            when service == "yahoo"
+                Yahoo.tag(text, token)
+            when service == "alchemy"
+                Alchemy.tag(text, token)
+            else
+                Tagthe.tag(text)
+        end
+    end
+
+    module_function :tag
+    module_function :get_token
+end #of webtagger module
